23975ce520233e27bc6832258212b817a504bced
AkashAbrrar <mabrrar9@gmail.com>
2022-03-29 11:18:51 +0600

    react simple bar and line charts for practicing

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MyLineChart from './coomponenets/MyLineChart/MyLineChart';
+import SpecialChart from './coomponenets/SpecialChart/SpecialChart';
+function App() {
+  return (
+    <div className="App">
+      <MyLineChart></MyLineChart>
+      <SpecialChart></SpecialChart>
+    </div>
+  );
+}
+
+export default App;
